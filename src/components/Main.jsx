2ace12456c11df9b01bd89b7a7274e8e1a5622ee
Md. Habibur Rahman <rhabibur@ghit.com>
2025-10-14 13:04:59 +0600

    added slider in the homepage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,45 @@
-
+import 'aos/dist/aos.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Main = () => {
 
     return (
-       <></>
+       <>
+               <div className='pt-[40px]'>
+
+            <div className="carousel max-h-[500px] w-ful">
+                <div id="slide1" className="carousel-item relative  w-full">
+                    <img
+                        src="https://i.ibb.co.com/Q7TyPWdL/slider4.jpg"
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src="https://i.ibb.co.com/xtL6kVB5/slider3.jpg"
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src="https://i.ibb.co.com/RTn6sRnF/Slider1.jpg"
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+            
+        
+        </div >
+       </>
     );
 };
 
